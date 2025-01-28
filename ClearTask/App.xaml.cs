4ff205191e20da23f5d0a805ff1f41789dc79dfa
wapitie <axel.overath@student.ehb.be>
2025-01-28 10:43:20 +0100

    start project

---
+++ ClearTask/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ClearTask
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ClearTask/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ClearTask
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
