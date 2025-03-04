5226e7e03fb4e3705217408e018cb612e48351eb
MatsDer123 <148542178+MatsDer123@users.noreply.github.com>
2025-03-05 00:41:21 +0100

    Description: -Added sector overview and crud -handyman from a sector and crud

--- ClearTask/Models/Sector.cs
+++ ClearTask/Models/Sector.cs
@@ -5,7 +5,6 @@ public class Sector
 {
     public ObjectId Id { get; set; }
     public string name { get; set; }
-    public List<ObjectId> taskIds { get; set; }
     public List<ObjectId> employeeIds { get; set; }
     public List<ObjectId> handymanIds { get; set; }
 }
